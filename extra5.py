c1a533171e8a24cc0d851642fdfc97e9de207872
Natalia Almeida <nat.almeida1989@gmail.com>
2026-03-17 15:59:26 -0300

    Delete extra5.py

--- extra5.py
+++
@@ -1,8 +0,0 @@
-senha_correta = "soufoda"
-senha = input("Digite sua senha: ")
-while senha!= senha_correta:
-    print("Senha incorreta! Tente novamente.")
-    senha = input("Digite sua senha: ")
-    
-print("Acesso permitido!")
-    

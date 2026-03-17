cd152ae33cc937e5a680cf690e7af07e968aa855
Natalia Almeida <nat.almeida1989@gmail.com>
2026-03-17 15:59:42 -0300

    Add files via upload

---
+++ semana-01-fundamentos/extra5.py
@@ -0,0 +1,8 @@
+senha_correta = "soufoda"
+senha = input("Digite sua senha: ")
+while senha!= senha_correta:
+    print("Senha incorreta! Tente novamente.")
+    senha = input("Digite sua senha: ")
+    
+print("Acesso permitido!")
+    
